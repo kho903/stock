8751ad1eba9c07d70aa20dd1204074d4096999f4
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-14 17:44:25 +0900

    feat: Optimistic Lock 활용해보기
    - 장점 : 별도의 Lock을 잡지 않아 Pessimistic Lock보다 성능상의 이점 존재.
    - 단점 : update 실패시 재시도 로직을 개발자가 직접 작성해야 함. 충돌이 빈번히 일어날 시 Pessimistic Lock이 성능상 더 좋을 수 있음.

---
+++ src/main/java/com/jikim/stock/service/OptimisticLockStockService.java
@@ -0,0 +1,25 @@
+package com.jikim.stock.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jikim.stock.domain.Stock;
+import com.jikim.stock.repository.StockRepository;
+
+@Service
+public class OptimisticLockStockService {
+
+	private StockRepository stockRepository;
+
+	public OptimisticLockStockService(StockRepository stockRepository) {
+		this.stockRepository = stockRepository;
+	}
+
+	@Transactional
+	public void decrease(Long id, Long quantity) {
+		Stock stock = stockRepository.findByIdWithOptimisticLock(id);
+		stock.decrease(quantity);
+
+		stockRepository.saveAndFlush(stock);
+	}
+}
