78749c685a9ce5fae0b6b0b6a366b5ffe9cbbbfe
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-14 16:51:05 +0900

    feat: synchronized 문제점

--- src/main/java/com/jikim/stock/service/StockService.java
+++ src/main/java/com/jikim/stock/service/StockService.java
@@ -17,6 +17,10 @@ public class StockService {
 
 	// @Transactional
 	public synchronized void decrease(Long id, Long quantity) {
+		/**
+		 * synchronized는 자바의 프로세스가 한 개 일때에만 보장.
+		 * 따라서 서버가 여러 대일 경우 보장할 수 없음.
+		 */
 		Stock stock = stockRepository.findById(id).orElseThrow();
 		stock.decrease(quantity);
 
